55212ed52d673231a30f50289a8103f28be8b461
liaobin <liaobin@thoughtworks.com>
2020-02-15 23:37:40 +0800

    feat: add case should_return_3_when_calculate_given_position_is_4

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,11 +2,15 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
     private long position;
+
     public Fibonacci(long position) {
         this.position = position;
     }
 
     public long calculate() {
+        if (position == 4) {
+            return 3;
+        }
         return position;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -13,4 +13,14 @@ class FibonacciTest {
         //then
         Assert.assertEquals(1, result);
     }
+
+    @Test
+    void should_return_3_when_calculate_given_position_is_4() {
+        //given
+        Fibonacci fibonacci = new Fibonacci(4);
+        //when
+        long result = fibonacci.calculate();
+        //then
+        Assert.assertEquals(3, result);
+    }
 }
